0125d06b23448a1ee3f6eba347d2d368ee78d1ac
emirkarakavak <emir.karakavak@gmail.com>
2025-09-10 15:01:49 +0300

    server side

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 app.use(express.static('public'))
 app.set("view engine", "twig");
-
+app.use(helmet({ contentSecurityPolicy: false }));
 app.disable("x-powered-by");
+app.set('trust proxy', true);
 
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
     res.render("index")
 }
 )
